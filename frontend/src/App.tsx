0bbc13a881f789cac0b5412c5cc3b5812f0e62c9
Ishan Garg <ishan@Ishans-MacBook-Air.local>
2024-09-25 19:14:28 +0530

    added vercel analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Landing } from './screens/Landing'
 import { Game } from './screens/Game'
+import { Analytics } from "@vercel/analytics/react"
+
 function App() {
   return (
     <>
@@ -10,6 +12,7 @@ function App() {
           <Route path="/game" element={<Game/>} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </>
   )
 }
